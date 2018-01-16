4ccffae13560e021039db41fce786680e2c43eff
Varun Wadhwa <vwadhwa3@gmail.com>
2018-01-16 17:34:17 +0530

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,31 @@
+canvas = document.getElementById("pixel_canvas");
+height = document.getElementById("input_height");
+width = document.getElementById("input_width");
+sizePicker = document.getElementById("sizePicker");
+color = document.getElementById("colorPicker");
+
+color.addEventListener("click", function(){});
+
+sizePicker.onsubmit = function(event){
+    event.preventDefault();
+    clearGrid();
+    makeGrid();
+};
+function makeGrid() {
+    for (let r=0; r<height.value; r++){
+        const row = canvas.insertRow(r);
+        for (let c=0; c<width.value; c++){
+            const cell = row.insertCell(c);
+            cell.addEventListener("click", fillSquare);
+        }
+    }
+}
+function clearGrid(){
+    while (canvas.firstChild){
+         canvas.removeChild(canvas.firstChild);
+    }
+}
+
+function fillSquare () {
+    this.setAttribute("style", `background-color: ${color.value}`);
+}
